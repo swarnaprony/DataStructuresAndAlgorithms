8327e1e31c6861796b777b5455f528d50fad7efa
swarnaprony <swarnaprony81@gmail.com>
2022-11-10 12:47:44 +0100

    update stacks.py to add stack_pop and stack_push

--- Python/stacks.py
+++ Python/stacks.py
@@ -17,6 +17,29 @@ class Stack:
             temp = temp.next
         return True
 
+    def stack_push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        if self.height > 0: 
+            new_node.next = self.top
+            self.top = new_node
 
-my_stack = Stack(2)
+        self.height += 1
+        return True
+
+    def stack_pop(self):
+        if self.height == 0:
+            return None
+        temp = self.top
+        self.top = temp.next
+        self.height -= 1
+        return temp
+
+
+my_stack = Stack(1)
+my_stack.stack_push(2)
+my_stack.stack_push(3)
+my_stack.stack_pop()
+my_stack.stack_pop()
 my_stack.print_stack()
